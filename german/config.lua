1cb4eb072354af2dd5138d240aca9418dc86409c
Byte <148918406+thisbyte@users.noreply.github.com>
2023-12-22 22:39:56 +0100

    Updated config

--- german/config.lua
+++ german/config.lua
@@ -1,6 +1,12 @@
 Config = {}
 
 --------------------------------------------------------------------------------
+---------------------------- GENUTZTES FRAMEWORK -------------------------------
+--------------------------------------------------------------------------------
+
+Config.Framework = "vorp" -- Als Framework werden "vorp" für VORP Core oder "rsg" für RSG Core unterstützt
+
+--------------------------------------------------------------------------------
 ---------------- NPC-UNTERTITEL BEI EINEM ERFOLGREICHEN HANDEL -----------------
 --------------------------------------------------------------------------------
 
@@ -58,6 +64,16 @@ Config.TradeNoSpace = {
 }
 
 --------------------------------------------------------------------------------
+--------- NPC-UNTERTITEL, BEI DER MAXIMALEN ANZAHL AN TAUSCHGESCHÄFTEN ---------
+--------------------------------------------------------------------------------
+
+Config.TradeQueueFull = {
+    "Wait a moment, I have my hands full right now.",
+    "You can see that I still have plenty to do. Just wait a moment.",
+    "I'm sorry, I can't take on any more right now."
+}
+
+--------------------------------------------------------------------------------
 ------ BEI DEAKTIVIERTEN NPC-UNTERTITELN, WERDEN DIESE MELDUNGEN GENUTZT ------- 
 --------------------------------------------------------------------------------
 
@@ -65,6 +81,7 @@ Config.TradeSuccessNotification = "Der Handel war erfolgreich!"
 Config.TradeFailNotification = "Dir fehlen die benötigten Gegenstände für diesen Handel!"
 Config.TradeNotAllowedNotification = "Du darfst hier nicht handeln!"
 Config.TradeNoSpaceNotification = "Du hast nicht genügend Platz in deinem Inventar."
+Config.TradeQueueFullNotification = "Die Liste ist voll, warte einen Moment für den nächsten Handel."
 
 --------------------------------------------------------------------------------
 ----------- ÜBERSETZUNGEN, WELCHE INNERHALB DES MENÜS GENUTZT WERDEN ----------- 
@@ -73,6 +90,35 @@ Config.TradeNoSpaceNotification = "Du hast nicht genügend Platz in deinem Inven
 Config.Translations = {
     ["needed"] = "wird benötigt.",
     ["receive"] = "Du erhältst",
+}
+
+--------------------------------------------------------------------------------
+------------------------------- DISCORD WEBHOOKS ------------------------------- 
+--------------------------------------------------------------------------------
+
+Config.UseWebhook = false
+Config.WebhookData = {
+    WebhookUrl = "",
+    WebhookTitle = "",
+    WebhookColor = 16711680,
+    WebhookName = "Barterer",
+    WebhookLogo = "https://via.placeholder.com/30x30",
+    WebhookLogo2 = "https://via.placeholder.com/30x30",
+    WebhookAvatar = "https://via.placeholder.com/30x30"
+}
+
+--[[ In den Discord-Nachrichten können Platzhalter verwendet werden, folgende Platzhalter sind verfügbar:
+
+[character_name] - Fügt den Charakternamen im Format "Vorname Nachname" ein
+[needed] - Eine Liste der Gegenstände die eingetauscht wurden
+[got] - Eine Liste der Gegenstände, welche der Spieler nach dem Tausch erhalten hat
+[trader_name] - Fügt den Namen des Händlers ein
+[trader_position] - Fügt die Koordinaten des Händlers zum Zeitpunkt des Handels im Format "x, y, z" ein
+
+]]
+
+Config.WebhookTranslation = {
+    ["tradeSuccess"] = "[character_name] hat [needed] für [got] eingetauscht."
 }
 
 --------------------------------------------------------------------------------
@@ -85,20 +131,33 @@ Config.Traders = {
     {
         id = "cottoneye", -- Eine einzigartige ID, welche intern vom Script genutzt wird
         name = "Cottoneye Joe", -- Der Name des Händlers (bzw. des NPC), welcher ebenso bei Interaktion angezeigt wird
+		randomizePosition = false, -- Wenn die Position dieses Händlers zufällig ausgewählt werden soll (Die Position wird aus den unten aufgeführten Listen gewählt) setze dies auf true, andernfalls auf false
         enableBlip = true, -- Möchtest du eine Markierung auf der Karte? Dann setze diesen Wert auf true, andernfalls auf false
         blipData = {
             icon = "blip_ambient_secret", -- Das Icon, welches für die Markierung genutzt wird
             name = "Cottoneye Joe", -- Der Name der Markierung auf der Karte
-            position = vector3(2336.84, -1519.23, 46.06) -- Die Position der Markierung
+            position = vector3(2336.84, -1519.23, 46.06), -- Die Position der Markierung
+			positionList = { -- Liste der möglichen Positionen, aus denen das Script eine auswählt (nur wenn randomizePosition aktiv ist)
+                vector3(2336.84, -1519.23, 46.06),
+                vector3(2346.9980, -1549.0118, 46.06),
+            },
         },
         enableNPC = true, -- Möchtest du einen NPC nutzen, setze diesen Wert auf true, andernfalls auf false
         npcData = {
             spawnDistance = 100.0, -- Ab welcher Entfernung der NPC despawned
             model = "MP_U_M_M_TRADER_01", -- Das Ped-Model des NPC
-            position = vector4(2336.84, -1519.23, 46.06, 115.71) -- Die Position des NPC
+            position = vector4(2336.84, -1519.23, 46.06, 115.71), -- Die Position des NPC
+			positionList = { -- Liste der möglichen Positionen, aus denen das Script eine auswählt (nur wenn randomizePosition aktiv ist)
+                vector4(2336.84, -1519.23, 46.06, 115.71),
+                vector4(2346.9980, -1549.0118, 46.06, 353.2121),
+            },
         },
         promptData = {
             position = vector3(2336.84, -1519.23, 46.06), -- Die Position an welcher das Interaktionsmenü angezeigt wird
+			positionList = { -- Liste der möglichen Positionen, aus denen das Script eine auswählt (nur wenn randomizePosition aktiv ist)
+                vector3(2336.84, -1519.23, 46.06),
+                vector3(2346.9980, -1549.0118, 46.06),
+            },
             distance = 2.0, -- Die Distanz zur oben genannten Position, ab welcher das Interaktionsmenü genutzt werden kann
             key = 0x760A9C6F, -- Die Taste, um mit diesem Händler zu interagieren
             text = "Ansprechen" -- Der Text, welcher zum Interagieren angezeigt wird
@@ -110,16 +169,17 @@ Config.Traders = {
         jobGradeNeeded = 0, -- Der Rang in den jeweiligen oben genannten Jobs, welcher zum Handeln benötigt wird
         messagesName = "Joe", -- Der Name, welcher in den NPC-Untertiteln angezeigt wird, zum Beispiel "Fremder"
         enableWelcomeMessage = true, -- Möchtest du, dass der Händler die Spieler willkommen heißt, wenn sie das Menü öffnen?
-        welcomeMessage = "How may i help you today?", -- Wie soll diese Willkommensnachricht aussehen?
+        welcomeMessage = "Was kann ich für dich tun?", -- Wie soll diese Willkommensnachricht aussehen?
         enableTradingMessages = true, -- Möchtest du die zufällig ausgewählten Nachrichten, welche einen Handel begleiten (siehe ganz oben), nutzen?
         trades = { -- Verfügbare Handelsangebote
             {
                 name = "Joe's Moonshine", -- Name des Angebots
-                image = "moonshine.png", -- Das Bild, das im Menü genutzt wird (Muss auf .png enden). Platziere es in /html/img/items
-                demand = { -- Es kann der Name jedes Items oder "money", "gold" oder "rol" genutzt werden
-                    { name = "water", image="water.png", label = "Water", amount = 2 } -- Name des Items in der Datenbank, platziere das zugehörige Bild in /html/img/items/, der Anzeigetext im Menü, die benötigte Anzahl
+				tradingTime = 0, -- Zeit welche der Tausch in Anspruch nehmen soll (in Sekunden)
+                image = "moonshine.png", -- Das Bild, das im Menü genutzt wird. Platziere es in /html/img/
+                demand = {
+                    { name = "water", image = "water.png", label = "Water", amount = 2 } -- Name des Items in der Datenbank, platziere das zugehörige Bild in /html/img/items/, der Anzeigetext im Menü, die benötigte Anzahl
                 },
-                offer = { -- Es kann der Name jedes Items oder "money", "gold" oder "rol" genutzt werden
+                offer = {
                     { name = "moonshine", label = "Moonshine", amount = 1 } -- Name des Items in der Datenbank, platziere das zugehörige Bild in /html/img/items/, der Anzeigetext im Menü, die Anzahl, welche der Spieler erhält
                 }
             }
